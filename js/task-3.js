5f2dd3f206381314b81d65b8c59f1d9092d80ba0
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-10-25 00:46:28 +0300

    Done

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function filterArray(numbers, value) {
+  let newArr = [];
+
+  for (const el of numbers) {
+    if (el > value) {
+      newArr.push(el);
+    }
+  }
+  return newArr;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
